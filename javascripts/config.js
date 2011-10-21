1ced0c7686e602849f43a6962abcd6e6cad37975
Brent Murphy <brent@wearestac.com>
2011-10-21 17:06:25 +0100

    Added url validation.

--- javascripts/config.js
+++ javascripts/config.js
@@ -4,5 +4,5 @@ Demo.Config = {
   url : "http://localhost:3001/",
   version : 1,
   userID : "4e9ecf252388cb2884000001",
-  appID : "4ea00a0d2388cb6a70000001"
+  appID : "4ea197522388cb185b000001"
 };
