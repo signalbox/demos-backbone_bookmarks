9352810f0ef95eaeb60cc4c35d85c434a46269f3
Brent Murphy <brent@wearestac.com>
2011-10-29 14:27:19 +0100

    Moved to names over ids for app context params.

--- javascripts/application.js
+++ javascripts/application.js
@@ -12,8 +12,8 @@ $(function() {
       var prefix = (settings.url.indexOf("?") > -1) ? "&" : "?";
       settings.url += prefix + $.param({
         sb_version : Demo.Config.version,
-        sb_user_id : Demo.Config.userID,
-        sb_app_id : Demo.Config.appID
+        sb_username : Demo.Config.username,
+        sb_app_name : Demo.Config.appName
       });
       xhr.setRequestHeader("Accept", "application/json");
       xhr.setRequestHeader("Content-Type", "application/json");
--- javascripts/config.js
+++ javascripts/config.js
@@ -1,8 +1,8 @@
 Namespace('Demo.Config');
 
 Demo.Config = {
-  url : "http://localhost:3001/",
+  url : "http://api.getsignalbox.dev:8080/",
   version : 1,
-  userID : "4e9ecf252388cb2884000001",
-  appID : "4ea2d6102388cb48ec000001"
+  username : "brent",
+  appName : "backbone_demo"
 };
