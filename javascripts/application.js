b87b9f8dd7b0d366915e1bcc654befd84d7c0fd7
Josh Nesbitt <josh@wearestac.com>
2012-04-12 14:27:20 +0100

    Changed params related to new Signal Box API.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,31 +1,33 @@
-$(function() {  
-  
+$(function() {
+
   // TODO: Move setup to an app object
   Handlebars.registerHelper('currentPage', function(page) {
     return this.page == page ? 'active' : '';
   });
-  
+
   $.ajaxSetup({
     cache : false,
     dataType : "json",
     beforeSend : function(xhr, settings) {
       var prefix = (settings.url.indexOf("?") > -1) ? "&" : "?";
+
       settings.url += prefix + $.param({
-        sb_version : Demo.Config.version,
-        sb_user_id : Demo.Config.userID,
-        sb_app_id : Demo.Config.appID
+        sb_version  : Demo.Config.version,
+        sb_username : Demo.Config.username,
+        sb_app_name : Demo.Config.appName
       });
+
       xhr.setRequestHeader("Accept", "application/json");
       xhr.setRequestHeader("Content-Type", "application/json");
     }
   });
-  
+
   var eventBus = _.extend({}, Backbone.Events);  // TODO: Remove if not in use
   var app = new Demo.Models.App();
   var appView = new Demo.Views.App({ model : app });
   var router = new Demo.Router(app);
   app.signOut();
-  
+
   Backbone.history.start({ pushState : false });
 
 });
--- javascripts/config.js
+++ javascripts/config.js
@@ -1,8 +1,8 @@
 Namespace('Demo.Config');
 
 Demo.Config = {
-  url : "http://localhost:3001/",
-  version : 1,
-  userID : "4e9ecf252388cb2884000001",
-  appID : "4ea2d6102388cb48ec000001"
+  url      : "http://api.getsignalbox.com/",
+  version  : 1,
+  username : "signalboxdemo",
+  appName  : "bookmarks"
 };
