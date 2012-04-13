88a1603b3c1078b1b5d73939d5ab960f79e44ccd
Josh Nesbitt <josh@wearestac.com>
2012-04-13 17:50:37 +0100

    Added urlRoot to user.

--- javascripts/models/user.js
+++ javascripts/models/user.js
@@ -1,5 +1,7 @@
 Demo.Models.User = Demo.Models.Base.extend({
 
-  serializable : [ '_id', 'username' ]
+  serializable : [ '_id', 'username' ],
+
+  urlRoot : Demo.Config.url + 'resources/users'
 
 });
